a70596a4c258c21cb034ffd717350655d07944da
Mikey Joyce <joyce.mp2019@gmail.com>
2020-03-03 12:53:21 -0600

    Declared the needed prototypes
    Declared all prototype functions needed for this library.

---
+++ fractionLib.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void fMultDiv(int , double , double , double , double , double *, double *);
+void fAddSub(int , double , double , double , double , double *, double *);
+void simplify(double *, double *);
+
+void fMultDiv(int flag, double n1, double d1, double n2, double d2, double *num, double* denom){
+
+}
+
+void fAddSub(int flag, double n1, double d1, double n2, double d2, double *num, double *denom){
+
+}
+
+void simplify(double *num, double *denom){
+
+}
